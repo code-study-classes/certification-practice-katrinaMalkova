43d802c5f9a5be81484fc06fa3dfd29a02b2e200
katrinaMalkova <kitty.malkova@gmail.com>
2023-11-01 11:35:09 +0300

    задание 123

--- numbers.js
+++ numbers.js
@@ -1,57 +1,37 @@
-//tsk1
+// tsk1
 
-const calculateDistance = (x1, x2) => Math.abs(x1 - x2);
+function calculateDistance(x1, x2) {
+  return Math.abs(x2 - x1);
+}
 
-const calculateSegmentProduct = (A, B, C) => {
-  const AClength = Math.abs(C - A);
-  const BClength = Math.abs(C - B);
-  return AClength * BClength;
-};
+console.log(calculateDistance(0, 5));
 
-const calculateKilobytes = (fileSizeInByte) => {
-  if (fileSizeInByte % 1024 === 0) {
-    return fileSizeInByte / 1024;
-  }
-  return Math.floor(fileSizeInByte / 1024);
-};
+export { calculateDistance };
 
-const calculateSegments = (lengthA, lengthB) => Math.floor(lengthA / lengthB);
+// tsk2
 
-const calculateDigitSum = (twoDigitNumber) => {
-  const arr = twoDigitNumber.toString();
-  return twoDigitNumber = parseInt(arr[0], 10) + parseInt(arr[1], 10);
-};
+function calculateSegmentProduct(A, B, C) {
+  return Math.abs((C - A) * (C - B));
+}
 
-const swapHundredsAndTens = (twoDigitNumber) => {
-  const arr = twoDigitNumber.toString();
-  return twoDigitNumber = parseInt((arr[1], 10) + parseInt(arr[0], 10) + parseInt(arr[2], 10), 10);
-};
+console.log(calculateSegmentProduct(0, 5, 2));
 
-const getHundredsDigit = (number) => {
-  if (number > 999) {
-    return Math.trunc(number / 100) % 10;
-  }
-  return 0;
-};
+export { calculateSegmentProduct };
 
-const getFullHours = (seconds) => {
-  if (typeof seconds !== 'number'  seconds < 0  !Number.isInteger(seconds)) {
-    throw ''; // mozilla//
-  }
-  const hours = Math.trunc(seconds / 3600);
-  return hours;
-};
+// tsk3
 
-const getDayOfWeek = (DayOfYear) => (((DayOfYear + 1) % 7) - 1);
+function calculateKilobytes(fileSizeInBytes) {
+  return Math.floor(fileSizeInBytes / 1024);
+}
 
-const countSquares = (A, B, C) => {
-  const squaresByW = Math.floor(A / C);
-  const squaresByH = Math.floor(B / C);
-  return squaresByW * squaresByH;
-};
+console.log(calculateKilobytes(2048));
 
-export {
-  calculateDistance, calculateSegmentProduct, calculateKilobytes, calculateSegments,
-  calculateDigitSum, swapHundredsAndTens, getHundredsDigit, getFullHours,
-  getDayOfWeek, countSquares,
-};
+export { calculateKilobytes };
+
+// tsk4
+
+function calculateDigitSum() {
+
+}
+
+export { calculateDigitSum };
